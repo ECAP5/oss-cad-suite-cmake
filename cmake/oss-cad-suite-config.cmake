f80ca6df49b2ada7529d850fcb24ceed8f67e413
Clément Chaine <clement.chaine25@gmail.com>
2025-11-28 22:50:37 +0100

    Apply sv2v before synthesis

--- cmake/oss-cad-suite-config.cmake
+++ cmake/oss-cad-suite-config.cmake
@@ -65,6 +65,24 @@ endif()
 
 set(oss_cad_suite_FOUND 1)
 
+# Prefer SV2V_ROOT from environment
+if (DEFINED ENV{SV2V_ROOT})
+  set(SV2V_ROOT "$ENV{SV2V_ROOT}" CACHE PATH "SV2V_ROOT")
+endif()
+
+set(SV2V_ROOT "${CMAKE_CURRENT_LIST_DIR}" CACHE PATH "SV2V_ROOT")
+if (NOT SV2V_ROOT)
+  message(FATAL_ERROR "SV2V_ROOT cannot be detected. Set it to the appropriate directory (e.g. /usr/share/sv2v) as an environment variable or CMake define.")
+endif()
+
+# Search for the binaries
+find_program(SV2V_BIN NAMES yosys
+  HINTS ${SV2V_ROOT}/bin ENV SV2V_ROOT
+  NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+if (NOT SV2V_BIN)
+  message(FATAL_ERROR "Cannot find sv2v executable.")
+endif()
+
 function(add_synthesis_target)
   cmake_parse_arguments(SYNTH "" # options
                               "LIB;OUTPUT;TARGET_FPGA;TOP_MODULE"     # one-value args
@@ -90,10 +108,18 @@ function(add_synthesis_target)
     message(FATAL_ERROR "Need a top module")
   endif()
 
+  set(FLAT_SOURCE_PATH ${CMAKE_CURRENT_BINARY_DIR}/${SYNTH_LIB}_flat.v)
+
+  add_custom_command(
+    OUTPUT ${FLAT_SOURCE_PATH}
+    COMMAND sv2v $<TARGET_PROPERTY:${SYNTH_LIB},INTERFACE_SOURCES> > ${FLAT_SOURCE_PATH}
+    DEPENDS ${SYNTH_LIB}
+    COMMAND_EXPAND_LISTS)
+
   add_custom_command(
     OUTPUT ${SYNTH_OUTPUT}
-    DEPENDS $<TARGET_PROPERTY:${SYNTH_LIB},INTERFACE_SOURCES>
-    COMMAND ${YOSYS_BIN} -p \'read -sv $<TARGET_PROPERTY:${SYNTH_LIB},INTERFACE_SOURCES> "\;" synth_${SYNTH_TARGET_FPGA} -top ${SYNTH_TOP_MODULE} -json ${SYNTH_OUTPUT}\'
+    DEPENDS ${FLAT_SOURCE_PATH}
+    COMMAND ${YOSYS_BIN} -p \'read -sv ${FLAT_SOURCE_PATH} "\;" synth_${SYNTH_TARGET_FPGA} -top ${SYNTH_TOP_MODULE} -json ${SYNTH_OUTPUT}\'
     COMMAND_EXPAND_LISTS)
 endfunction()
 
